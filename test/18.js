7fb04627f202a99186e6cfa47ae788983fd1f954
maxtaco <max@m8api.com>
2011-06-30 19:26:22 -0400

    add the last few .js tests, we're going to rename them to .tjs

---
+++ test/18.js
@@ -0,0 +1,32 @@
+
+
+function x () {
+    while (i) {
+	if (j) {
+	    while (p) {
+		p++;
+	    }
+	    for (q in l) {
+		if (p) {
+		    return;
+		}
+	    }
+	}
+    }
+};
+
+
+function x () {
+    while (i) {
+	if (j) {
+	    while (p) {
+		p++;
+	    }
+	    for (q in l) {
+		if (p) {
+		    twait { kk(mevent()); }
+		}
+	    }
+	}
+    }
+};
