c14f1005a10483b408f623389340a4d7360abef9
maxtaco <max@m8api.com>
2011-07-05 11:54:20 -0400

    more clean up and add some new regtests

--- test/6.js
+++
@@ -1,5 +0,0 @@
-{
-    var bar = function () {};
-    bar ();
-    twait { x(); }
-}
--- test/7.js
+++
@@ -1,9 +0,0 @@
-
-function x (i) {
-    for (var j = 0; j < i; i++) {
-	if (j) {
-	    j++;
-	}
-    }
-}
-x (10);
