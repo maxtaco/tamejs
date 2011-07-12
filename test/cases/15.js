82354a1fda9b52c31248e9472cc27e9b3c342240
Maxwell Krohn <themax@gmail.com>
2011-07-12 12:08:25 -0400

    add an extension to node so that '.tjs' modules don't need to be explicitly compiled.

---
+++ test/cases/15.js
@@ -0,0 +1,6 @@
+// -*- mode: javascript; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil; -*-
+
+require ('tamejs').register ();
+var mod = require ("./lib/15.tjs");
+
+mod.makeTest (exports);
