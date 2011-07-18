16ae5f6dcf3d3c81c1c9f0f49812842d0e531aa2
Chris Coyne <ccoyne77@gmail.com>
2011-07-18 10:30:03 -0400

    brochure website for tamejs, first real version. could use a redesign.

---
+++ web/js/tamejs_site.js
@@ -0,0 +1,47 @@
+$(function() {
+	prettyPrint(); // prettify JS
+	$("#floating_container").floatingFixed();
+});
+
+//----------------------------------------
+// jquery extension to float elements
+//----------------------------------------
+
+(function($) {
+  var triggers = [];
+  $.fn.floatingFixed = function(options) {
+    options = $.extend({}, $.floatingFixed.defaults, options);
+    var r = $(this).each(function() {
+      var $this = $(this), pos = $this.position();
+      $this.data("floatingFixedTop", pos.top);
+      $this.data("floatingFixedLeft", pos.left);
+      $this.data("floatingFixedOptions", options);
+      $this.data("floatingFixedTopOrigTop", $this.css("top"));
+      triggers.push($this);
+    });
+    windowScroll();    
+    return r;
+  }
+  
+  $.floatingFixed = $.fn.floatingFixed;
+  $.floatingFixed.defaults = {
+    padding: 0
+  }
+  
+  var $window = $(window);
+  var windowScroll = function() {
+    if(triggers.length === 0) { return; }
+    var scrollY = $window.scrollTop();
+    for(var i = 0; i < triggers.length; i++) {
+      var t = triggers[i], opt = t.data("floatingFixedOptions"), top = t.data("floatingFixedTop");
+      if(top < scrollY + opt.padding && !t.data("isFloating")) {        
+//        t.css({position: 'fixed', top: opt.padding, left: t.data("floatingFixedLeft") }).data("isFloating", true);
+        t.css({position: 'fixed', top: opt.padding }).data("isFloating", true);
+      } else if(top >= scrollY + opt.padding && t.data("isFloating")) {
+        t.css({position: 'static', top: null, left: null}).data("isFloating", false);
+      }
+    }
+  };
+  
+  $window.scroll(windowScroll).resize(windowScroll);
+})(jQuery);
