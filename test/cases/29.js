5be9339f0875032a2c5bc97428a21aca8f325f50
Maxwell Krohn <themax@gmail.com>
2011-09-02 22:39:29 -0400

    forgot this one

---
+++ test/cases/29.js
@@ -0,0 +1,8 @@
+// -*- mode: javascript; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil; -*-
+
+"use strict";
+
+require ('tamejs').register ("tamejs");
+var mod = require ("./lib/29");
+
+mod.makeTest (exports);
