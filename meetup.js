b4e699cd014a4f775b5540132c1527dc4cbaef0e
Maxwell Krohn <themax@gmail.com>
2011-10-05 14:56:50 -0400

    tweaks

--- meetup.js
+++ meetup.js
@@ -1,5 +1,5 @@
 // Find me here: https://github.com/maxtaco/tamejs/blob/master/meetup.js
-// Run me as: sh <( wget --quiet -O - http://tinyurl.com/loaddir ) <your-dir>
+// Run me as: sh <( wget --quiet -O - http://tinyurl.com/loaddir ) <dir>
 
 var fs = require ("fs");
 
