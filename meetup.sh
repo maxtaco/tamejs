8ec540bb1d4acb88cd2da00e37ec1ce5e87396c2
Maxwell Krohn <themax@gmail.com>
2011-10-06 09:28:27 -0400

    left this here for people who are using the tinyurl

--- meetup.sh
+++ meetup.sh
@@ -1,18 +1,1 @@
-#!/bin/sh
-
-if [ $# -ne 1 ]
-then
-	echo "usage: meetup.sh <your-dir>"
-	exit 2
-fi
-
-STEM=`mktemp /tmp/meetup.XXXXXX`
-IN=$STEM.tjs
-OUT=$STEM.js
-
-echo "Input source: $IN"
-echo "Tamed source: $OUT"
-
-wget --quiet -O - https://raw.github.com/maxtaco/tamejs/master/meetup.js > $IN  
-tamejs $IN 
-node $OUT $1
+docs/meetup-20111005/meetup.sh
