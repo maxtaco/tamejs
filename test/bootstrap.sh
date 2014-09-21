406e0a6985ce7dc1dc1ac92c047c87022b3fd7c2
Kendrick Taylor <kendrick@circuitsix.com>
2014-09-21 17:57:17 +0200

    fix bug in bootstrap.sh if node_modules doesn't exist

--- test/bootstrap.sh
+++ test/bootstrap.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+mkdir $DIR/../node_modules
 ln -s $DIR/../ $DIR/../node_modules/tamejs
 node $DIR/harness.js
 rm $DIR/../node_modules/tamejs
